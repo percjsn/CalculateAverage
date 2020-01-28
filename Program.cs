b49ffb7c0e942c9db500b9e8d4ee82d9117e7819
jxvisn <jxvisn@gmail.com>
2020-01-28 08:24:06 -0800

    Made the app.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,23 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a set of numbers: "); // Ask for input
+            var input = Console.ReadLine(); // Store the input somewhere
 
+            // Evaluate Input
+            var array = input.Split(','); //Seperate on comma and turn into array
 
+            var totalNumbers = array.Length; //Get total numbers
+            var totalSum = 0; //Initialize the sum of all numbers
+
+            foreach(var i in array)
+            {
+                totalSum += Convert.ToInt32(i); //Add all the numbers up to the sum
+            }
+
+            //Then we need to divide the sum of all the numbers by the amount of numbers given
+            var average = totalSum / totalNumbers;
+            Console.WriteLine(average); // Print result
         }
     }
 }
